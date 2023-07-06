9a591356d7915c6f16a37b237c8958492604cd18
Luis Ángel Velázquez Palomino <angelvelazquez98@hotmail.com>
2023-07-06 14:20:06 -0600

    change server test

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 // server.js
-const express = require('express');
+const express = require("express");
 // Our DB Configuration
-require('./src/database');
+//require('./src/database');
 
 // Routes
-const postRouter = require('./src/routes/post.router');
+//const postRouter = require("./src/routes/post.router");
+const testRouter = require("./src/routes/test.router");
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send("Hello World ! ");
+app.get("/", (req, res) => {
+  res.send("Hello World ! ");
 });
 
 app.listen(PORT, function () {
-    console.log(`Server Listening on ${PORT}`);
+  console.log(`Server Listening on ${PORT}`);
 });
 
-app.use('/posts', postRouter);
+//app.use("/posts", postRouter);
 
-app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, '../client','build','index.html'));
-});
+app.use("/test", testRouter);
+
+// app.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, '../client','build','index.html'));
+// });
---
+++ src/routes/test.router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const testRouter = express.Router();
+
+/* Add Single Post */
+testRouter.post("/", (req, res, next) => {
+  console.log(req.body);
+
+  if (req.body) {
+    res.status(200).send({
+      success: true,
+      message: "Post created successfully"
+    });
+  }
+});
+
+module.exports = testRouter;
