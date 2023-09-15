367a8ecc0593cc634e9660018df0e02b313f6362
L0Angel98 <75176852+L0Angel98@users.noreply.github.com>
2023-09-14 20:36:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Habilitar CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://tlbtzrb3-5173.usw3.devtunnels.ms');
+  // Otros encabezados de CORS según tus necesidades
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World ! ");
 });
